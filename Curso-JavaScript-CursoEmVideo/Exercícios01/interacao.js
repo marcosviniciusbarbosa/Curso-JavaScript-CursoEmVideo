160cf83848d6a340d48cbd86e4b60f442fefd474
marcosviniciusbarbosa <marcosvb-oficial@outlook.com>
2020-06-29 09:40:15 -0300

    volta

--- Curso-JavaScript-CursoEmVideo/Exercícios01/interacao.js
+++ Curso-JavaScript-CursoEmVideo/Exercícios01/interacao.js
@@ -1,16 +1,17 @@
 function carregar(){
     var meg = document.querySelector('#texto')
-    var img = document.querySelector('#imagem')
+    var img = document.querySelector('#foto')
     var agora = new  Date()
     var hora = agora.getHours()
-    meg.innerHTML = `Agora são ${hora} horas` 
-    
-    if ( hora >= 0 && hora < 12 ){
-        img.src = 'manhã.jpg'
-    } else if(hora >= 12 && hora < 18){
-        img.src = 'tarde.jpg'
+    var min = agora.getMinutes()
+    var seg = agora.getSeconds()
+    meg.innerHTML = `${hora} Hrs ${min} Min ` 
+    if ( hora >= 0 && hora<12 ){
+        img.src = './image/manhã.jpg'
+    } else if(hora >= 12 && hora<18){
+        img.src = './image/tarde.jpg'
     } else {
-        img.src = 'noite01.jpg'
+        img.src = './image/noite01.jpg'
     }
 
 } 
---
+++ Curso-JavaScript-CursoEmVideo/Exercícios02/interacao.js
@@ -0,0 +1,8 @@
+function carregar(){
+    var idNas = document.querySelector('.idade')
+    var sexo = document.querySelector('.sexo')
+    var agora = new  Date()
+    var ano = agora.getFullYear
+    var idade = ano - idNas
+    if(idade )
+}
